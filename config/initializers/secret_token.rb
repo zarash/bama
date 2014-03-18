bc0175cff2fed20191d423597d55b14338481875
zara <zara@zara.(none)>
2014-03-19 01:17:11 +0330

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Otoyabi::Application.config.secret_key_base = '36a874854888c42b9484e5f2e186ff2e6a8bba2d3aa14084c13e3fa242545b0795caf81a505eaa10561440e1162c2db8202e927a8a7ddecdb31111e4192f9d84'
