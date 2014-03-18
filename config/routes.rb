f7a12a851828720613b0824886339267e6a5c00c
zara <zara@zara.(none)>
2014-03-19 02:27:26 +0330

    create scaffold brand

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Otoyabi::Application.routes.draw do
+  resources :brands
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
